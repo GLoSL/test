25f96580c5fffaf572c364776829e4c155eb0393
GLoSL <felix.og@laposte.net>
2016-12-02 10:32:30 +0100

    mayrd1

--- app.js
+++ app.js
@@ -11,21 +11,18 @@ function isIsogram(str){
   };
 
 
-  function isIsogram(str){
-  //...
-  str.toLowerCase();
-  for (var i = 0; i < str.length; i++) {
-    for (var j = i; j < str.length; j++) {
-    if (j == i) {
-      return false;
-      }
-    else {
-      return true;
-      }
-    };
+    console.log(str);
   }
 
-
-
-  console.log(str);
-}
+  function isIsogram(str){
+    //...
+    let lower = str.toLowerCase();
+    for (let i = 0; i < lower.length; i++) {
+      for (let j = i + 1; j < lower.length; j++) {
+      if (lower[j] === lower[i]) {
+        return false;
+        }
+      };
+    };
+    return true;
+    console.log(str);
